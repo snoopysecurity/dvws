6445786cbb60d13d86012e6e8b6c2b0171cbec28
Febriyanto Nugroho <mpep@outlook.co.id>
2016-02-21 14:37:02 +0700

    Update api.php

--- vulnerabilities/sqli/api.php
+++ vulnerabilities/sqli/api.php
@@ -8,7 +8,9 @@ $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
  
 // connect to the mysql database
-$link = mysql_connect('localhost', 'root', '', 'dvws');
+// if you use the lamp, you must enter a password mysql server
+// example password is @!=56u, you should change your password you use :)
+$link = mysql_connect('localhost', 'root', '@!=56u', 'dvws'); 
 mysql_set_charset('utf8');
  
 // retrieve the table and key from the path
